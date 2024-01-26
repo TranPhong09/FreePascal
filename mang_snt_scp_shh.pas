49f89765954cebe9631ea8766fa46ec16efa4f3c
TranPhong09 <151003788+TranPhong09@users.noreply.github.com>
2024-01-26 04:38:01 -0800

    Update mang_snt_scp_shh.pas

--- mang_snt_scp_shh.pas
+++ mang_snt_scp_shh.pas
@@ -7,7 +7,20 @@ uses crt ;
         if n mod i = 0 then d := d + 1 ;
         if d = 2 then snt := true else snt := false ;
    end;
-
+function scp(n : integer): boolean ; 
+ var x : integer ;
+  begin 
+    x:= trunc(sqrt(x)); 
+    if sqr(x) = n then scp := true else scp := false ; 
+  end; 
+ function shh ( n : integer ): boolean ; 
+   var i , d : integer ; 
+     begin 
+       d := 0; 
+       for i := 1 to n-1 do 
+         if ( n mod i = 0 ) then d :=d + i ; 
+         if d = n then shh := true else shh := false ; 
+end; 
 type arint = array[1..100] of integer;
 var fi , fo : text ;
    a : arint ;
@@ -22,7 +35,11 @@ begin
   assign(fo,'e:\freepascal\nhap.out.txt');
   rewrite(fo);
   for i := 1 to n do
+    begin 
     if snt(a[i]) = true then write(fo,'cac so nguyen to co trong mang la :',a[i],' ');
+    if scp(a[i]) = true then write(fo,' cac so chinh phuong co trong mang la :',a[i],' ');  
+    if chh(a[i] = true then write(fo,' cac so hoan hao co trong mang la :',a[i],' '); 
+end; 
  close(fo);
 
  readln
